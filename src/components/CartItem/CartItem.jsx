5e889c9ac65626c87e863dd0d2eaed86bdeda35a
Tania Ludueña <Tanialuduenaok@gmail.com>
2023-09-22 21:53:27 -0300

    Grabo ultimos cambios

--- src/components/CartItem/CartItem.jsx
+++ src/components/CartItem/CartItem.jsx
@@ -1,23 +1,19 @@
-import { useContext } from 'react';
-import { CartContext } from '../../context/CartContext';
-import './CartItem.css';
+import { useContext } from "react";
+import { CartContext } from "../../context/CartContext";
+import './CartItem.css'
 
-const CartItem = ({ id, name, price, img, quantity }) => {
+const CartItem = (item) => {
+
     const { removeItem } = useContext(CartContext);
 
-    return (
-        <div className="container cartCardd d-flex col-12 justify-content-around">
-            <img src={`/img/${img}`} alt={name} className="imageItem1" />
-            <h3 className="card-title text-center cardTitle">{name}</h3>
-            <p className="card-text cardText">${price}</p>
-            <p className="card-text cardText">Cantidad: {quantity}</p>
-            <button onClick={() => removeItem(id)} className="btn btn-danger cartRemove">
-                x
-            </button>
-        </div>
-    );
+    return <div className="container cartCardd d-flex col-12 justify-content-around">
+        <img src={`/img/${item.img}`} alt={item.name} className="imageItem1" />
+        <h3 className="card-title text-center cardTitle">{item.name}</h3>
+        <p className="card-text cardText">${item.price}</p>
+        <p className="card-text cardText">Cantidad: {item.quantity}</p>
+        <button onClick={() => removeItem(item.id)} className="btn btn-danger cartRemove">x</button>
+    </div>;
 };
 
 export default CartItem;
-
 
